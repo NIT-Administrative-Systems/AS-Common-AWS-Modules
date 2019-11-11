10e778fb9e19b680cb11f229770b1e1ef80438da
Nick Evans <nick.evans@northwestern.edu>
2019-11-11 15:48:07 -0600

    Adjust SAN cert slicing
    It was `list(var, start inclusive, stop exclusive)`, so I think that's slicing the last value off!

--- entapp_certificate/acm.tf
+++ entapp_certificate/acm.tf
@@ -1,6 +1,6 @@
 resource "aws_acm_certificate" "certificate_request" {
   domain_name       = "${var.hostnames[0]}"
-  subject_alternative_names = ["${slice(var.hostnames, 1, length(var.hostnames) - 1)}"]
+  subject_alternative_names = ["${slice(var.hostnames, 1, length(var.hostnames))}"]
   validation_method = "DNS"
 
   tags {
