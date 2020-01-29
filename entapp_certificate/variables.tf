712ba1824fcf03f48f540e36a6afb774512026a5
Nick Evans <nick.evans@northwestern.edu>
2020-01-29 16:09:21 -0600

    TF 0.12 upgrade for entapp cert

--- entapp_certificate/variables.tf
+++ entapp_certificate/variables.tf
@@ -1,4 +1,5 @@
 variable "hostnames" {
   description = "The domain name(s) that the site will be accessible as, e.g. our-register-dev.northwestern.edu. A SAN cert will be generated if you specify more than one hostname, with the first as the common name."
-    type = "list"
+  type        = list(string)
 }
+
