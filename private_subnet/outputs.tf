a5a0cd9e0dd2662135466b58a8adce48ca883f6d
Nick Evans <nick.evans@northwestern.edu>
2020-02-29 10:13:57 -0600

    Revert "Move enabled check deeper in"
    This reverts commit 6d7c1335a4b8f06d5ea42d67ffc59f95ab6a04c3.

--- private_subnet/outputs.tf
+++ private_subnet/outputs.tf
@@ -1,4 +1,4 @@
 output "subnet_id_list" {
-  value = [element((var.enabled == "true" ? aws_subnet.subnet_az1.*.id : []), 1), element((var.enabled == "true" ? aws_subnet.subnet_az2.*.id : []), 1)]
+  value = var.enabled == "true" ? [element(aws_subnet.subnet_az1.*.id, 1), element(aws_subnet.subnet_az2.*.id, 1)] : []
 }
 
