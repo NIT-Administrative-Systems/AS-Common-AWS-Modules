0e77615ebd1606e4bb945324dd76c2373abb48f2
Nick Evans <nick.evans@northwestern.edu>
2020-04-08 14:12:24 -0500

    Shared ALB security group should be a wildcard inbound

--- alb/alb.tf
+++ alb/alb.tf
@@ -23,9 +23,9 @@ resource "aws_security_group" "lb_security_group" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
