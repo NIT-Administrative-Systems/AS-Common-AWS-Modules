f7e06934eac90ab3b10cc58724e41e61b8af6f8e
Michael Campbell <michael.campbell@northwestern.edu>
2022-03-11 13:03:46 -0600

    fixed var reference to local

--- private_subnet_v2/subnets.tf
+++ private_subnet_v2/subnets.tf
@@ -52,7 +52,7 @@ resource "aws_route_table_association" "route_mappings" {
   
   //  this monstrosity takes the map of map of subnets, finds the subnet map corresponding to count.index, and isolates its id
   //  to visualize: lookup({{map_one}, {map_two}}, map_one, default) gets us to map_one = {key:value} so we do another lookup to get the value of the the key "id"
-  subnet_id = lookup(lookup(aws_subnet.subnets, keys(aws_subnet.subnets)[count.index], var.Default), "id", count.index)
+  subnet_id = lookup(lookup(aws_subnet.subnets, keys(aws_subnet.subnets)[count.index], local.Default), "id", count.index)
   route_table_id = lookup(element(aws_route_table.route_tables, count.index), "id", count.index)
 }
 
